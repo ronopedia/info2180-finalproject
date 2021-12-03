d2470d6ca93311e596d8744dc8216894a1196b11
ronopedia <81117989+ronopedia@users.noreply.github.com>
2021-12-02 21:30:28 -0500

    Update issue.php

--- info2180-finalproject/scripts/issue.php
+++ info2180-finalproject/scripts/issue.php
@@ -3,12 +3,13 @@ session_start();
 require_once 'dbconfig.php';
 
 try{
+    //if(isset($_POST['title'], $_POST['description'], $_POST['assigned_to'], $_SESSION['id'], $_POST['type'], $_POST['priority'])){
     $title = $_POST['title'];
-    $description = $_POST['description'];
-    $assign = $_POST['assigned_to'];
+    $description = $_POST['descrip'];
+    $assign = $_POST['assigned'];
     $created = $_SESSION['id'];
-    $typeof = $_POST['type'];
-    $priority = $_POST['priority'];
+    $typeof = $_POST['typeof'];
+    $priority = $_POST['pri'];
     $datetime = date("Y-m-d");
     $stat ='Open';
 
@@ -34,13 +35,17 @@ try{
 
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $stmt = $conn->query("SELECT id, firstname, lastname FROM users");
-    $insertdata = "INSERT INTO issues(title,description,type,priority,status,assigned_to,created_by,created,updated) VALUES('$title','$descrip','$typeof','$priority','$stat','$assign','$created',NOW(),NOW())";
+    $insertdata = "INSERT INTO issues(title,description,type,priority,status,assigned_to, created_by,created,updated) VALUES('$title','$description','$typeof','$priority','$stat','$assign','$created',NOW(),NOW())";
     $stmt = $conn->query($insertdata);
 
     $referer = $_SERVER['HTTP_REFERER'];
     header("Location: $referer");
     exit;
 
+    //}else{
+        //header("location: ../login.html");
+        //echo "User must log in to create an issue.";
+    //}
 
 }
 catch (PDOException $pe) {
@@ -49,3 +54,4 @@ catch (PDOException $pe) {
 
 
 ?>
+
